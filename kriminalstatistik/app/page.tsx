bbf3b349a0a79fb6207e73ccaf261d4d582d0888
fst135207 <felix.stalder.bsu@gmail.com>
2025-03-06 15:16:51 +0100

    Start of Project added Titles

---
+++ kriminalstatistik/app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-xl">Kriminalstatistik Bern</h1>
+
+      <p>Interaktive Karte mit Heatmaps</p>
+
+      <p>Zeitreihen- und Trenddiagramme</p>
+
+      <p>Filter und Vergleichsfunktionen</p>
+
+    </div>
+  );
+}
