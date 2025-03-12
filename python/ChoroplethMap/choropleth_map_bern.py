18fb8f47d3472061b242c573bae7805a19aba83b
fst135207 <felix.stalder.bsu@gmail.com>
2025-03-12 14:04:53 +0100

    direkt anfrage an das geojson erstellt & status.code zu status_code umgestellt da dies die korrekt bennenung ist

--- python/ChoroplethMap/choropleth_map_bern.py
+++ python/ChoroplethMap/choropleth_map_bern.py
@@ -5,18 +5,18 @@ https://data.opendatasoft.com/explore/dataset/georef-switzerland-kanton
 import requests
 
 def get_map_data():
-    url = "/api/explore/v2.1/catalog/datasets/georef-switzerland-kanton@public/records?refine=kan_name%3A%22Bern%22"
-
+    url = "https://public.opendatasoft.com/api/records/1.0/search/?dataset=georef-switzerland-kanton&q=kan_name%3A%22Bern%22&format=geojson"
+    
     try:
         # GET-Request an den API Endpoint
         response = requests.get(url)
 
         # Checken ob der Request erfolgreich war
-        if response.status.code == 200:
+        if response.status_code == 200:
             map_data = response.json()
             return map_data
         else:
-            print("Error: " + response.status.code)
+            print("Error: " + response.status_code)
             return None
         
 # Wir fügen eine Expectetion hinzu damit wir fehler die mit dem Netzwerk zu tuen haben ausfiltern können
@@ -27,6 +27,13 @@ def get_map_data():
 
 def main():
     map_data = get_map_data()
-    
+
+    if map_data:
+        print("Map Daten erfolgreich erhalten")
+    else:
+        print("Keine Daten erhalten...")
+
+if __name__ == "__main__":
+    main()
 
 
